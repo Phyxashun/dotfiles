bfefe8a5fba6f7ea9f2d9ba525b6670b608bb19f
Dustin Dew <phyxashun@gmail.com>
2025-11-05 23:02:21 -0500

    Update dotfiles

--- .bin/update-dotfiles.sh
+++ .bin/update-dotfiles.sh
@@ -12,16 +12,22 @@ NC='\033[0m'
 # Navigate to the directory of this script (generally ~/.dotfiles/.bin)
 cd $(dirname $(readlink -f $0))
 cd ..
+echo
 
+# Update the dotfiles from remote
 echo -e "${BLUE}Adding all files to git...${NC}"
 git add .
-
-echo -e "${BLUE}Committing updates to repo...${NC}"
 echo
+
+# Commit and push changes
+echo -e "${BLUE}Committing updates to repo...${NC}"
 git commit -m "Update dotfiles"
+echo
+
+echo -e "${BLUE}Pushing updates to remote repo...${NC}"
 git push origin $BRANCH
 echo
 
-
-# Run stow to ensure all new dotfiles are linked
+# Run stow to link dotfiles
+echo -e "${BLUE}Running stow to link dotfiles...${NC}"
 stow -vt ~ */
